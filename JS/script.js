c962e3bcbb8af130b0d67b5d83ebb51226937484
Davi Nascimento <127548240+davin4sciment0@users.noreply.github.com>
2023-04-10 22:53:42 -0300

    início_lista

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+let A = Number(prompt("Digite o valor de A: "));
+let B = Number(prompt("Digite o valor de B: "));
+let C = Number(prompt("Digite o valor de C: "));
+
+teste = A + B + C;
+console.log(`O resultado da conta é: ${teste}`)
